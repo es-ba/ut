3b66160567981b2e7629e5e692fcb8a500248127
GracielaCanessa <gracane@gmail.com>
2023-06-07 12:50:08 -0300

    subo versión.

--- src/server/app-ut.ts
+++ src/server/app-ut.ts
@@ -18,7 +18,7 @@ import { visitas_sup         } from './table-visitas_sup';
 import { hogares_sup         } from './table-hogares_sup';
 import { personas_sup        } from './table-personas_sup';
 
-const APP_DM_VERSION="#23-06-05";
+const APP_DM_VERSION="#23-06-07";
 
 export type Constructor<T> = new(...args: any[]) => T;
 export function emergeAppUt<T extends Constructor<dmencu.AppAppDmEncuType>>(Base:T){
