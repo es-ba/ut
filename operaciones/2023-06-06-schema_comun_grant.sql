4d4d85a5a2e6b4514c3e85669233804bc9f6ddd3
Raquel Salva <raquelzssalva@gmail.com>
2023-06-07 12:58:39 -0300

    Agregar permiso al admin  en schema comun. Agregar funcion update_varcal inicial

---
+++ operaciones/2023-06-06-schema_comun_grant.sql
@@ -0,0 +1,2 @@
+
+GRANT ALL ON SCHEMA comun TO ut2023_muleto_admin;
--- operaciones/inicial_update_varcal_por_encuesta.sql
+++ operaciones/inicial_update_varcal_por_encuesta.sql
@@ -17,3 +17,17 @@ $BODY$;
 
 ALTER FUNCTION base.update_varcal_por_encuesta(text, text)
     OWNER TO ut_admin;
+
+CREATE OR REPLACE FUNCTION base.update_varcal(
+	p_operativo text)
+    RETURNS text
+    LANGUAGE 'plpgsql'
+AS $BODY$
+BEGIN
+
+  RETURN 'OK';
+END;
+$BODY$;
+
+ALTER FUNCTION base.update_varcal(text)
+    OWNER TO ut_admin;
