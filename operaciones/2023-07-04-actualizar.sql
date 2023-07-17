aba148933697aa20836aba8825e5b05b456e48fa
manueldelapenna <manueldelapenna@gmail.com>
2023-07-17 15:19:41 -0300

    ajustes varios

--- operaciones/2023-07-04-actualizar.sql
+++ operaciones/2023-07-04-actualizar.sql
@@ -117,4 +117,37 @@ from (select *
         where operativo = 'UT_2023' and verificado = '1') as tt
 where te.operativo = tt.operativo and te.enc = tt.enc and te.tarea_actual = tt.tarea and  tarea_proxima is null;
 
-update tareas_tem set ts_entrada = '2023-07-10 00:00:01' where operativo = 'UT_2023' and tarea = 'finc';
+--update tareas_tem set ts_entrada = '2023-07-10 00:00:01' where operativo = 'UT_2023' and tarea = 'finc';
+
+CREATE OR REPLACE FUNCTION accion_cumple_condicion(p_operativo text, p_estado text, p_enc text, p_eaccion text, p_condicion text)
+RETURNS boolean AS
+$BODY$
+DECLARE
+    v_sent text; 
+    v_cond text;
+    v_salida integer;
+BEGIN
+ v_cond=p_condicion;
+ v_sent=' select 1 
+    from base.tareas_tem t
+    inner join base.estados_acciones ea using (operativo, estado)
+    inner join tem te using (operativo,enc)
+    left join tokens tok on t.cargado_dm=tok.token
+	  left join viviendas v on (te.operativo =  v.operativo and te.enc = v.vivienda)
+    left join no_rea nr on (te.norea::text = nr.no_rea)
+    where t.operativo='||quote_literal(p_operativo)||
+    ' and t.estado='||quote_literal(p_estado)||
+    ' and t.enc='||quote_literal(p_enc)||
+    ' and ea.eaccion='||quote_literal(p_eaccion)||
+    ' and '||v_cond||';';
+ --raise notice 'esto %',vsent;
+ execute v_sent into v_salida;
+ IF v_salida=1 THEN
+    return true;
+ ELSE
+    return false;
+ END IF;
+
+END;
+$BODY$
+ LANGUAGE plpgsql VOLATILE;
